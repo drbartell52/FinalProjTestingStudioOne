02a548282e429291b51f0b4cbb57ea80d1a2dc77
drbartell52 <123522462+drbartell52@users.noreply.github.com>
2023-12-06 12:03:14 -0500

    Further Implementation of Logic (& Comments)
    Capacity now changes when buildings are placed. I added comments to all the code in case y'all need to understand how any of it works :)

--- Assets/Scripts/PodiumMechanics/SnapPlacement.cs
+++ Assets/Scripts/PodiumMechanics/SnapPlacement.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class SnapPlacement : MonoBehaviour
 {
+    //For now, this script is intended to be put on the trigger that is placed where you want the object to go while the
+    //grabbed building is within the trigger, the user can press a button to snap the building into it's final location
+    
+    //Location the object snaps to
     public Transform objectPlacement;
+
+    //Assign other object with life capacity script to this in editor (one-way dependency)
+    public LifeCapacity lifeCapacity;
 
     private void OnTriggerStay(Collider other)
     {
+        //Jump should/will be changed to input on Oculus controller
         if (Input.GetButtonDown("Jump"))
         {
+            //Add tag "Building" To the grabbable building objects
             if (other.CompareTag("Building"))
             {
+                //Stops buildings from flying away after placement (if gravity is turned off in rigidbody)
                 other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-                //other.transform.position = objectPlacement.position;
+                
+                //Sets placement of the grabbed building to the location chosen, height is determined by dividing
+                //building scale by 2, the "+ 6" can be changed to whatever the height of the podium is
                 other.transform.position = new Vector3(objectPlacement.position.x,
                     (other.transform.lossyScale.y/2) + 6, objectPlacement.position.z);
+                
+                //To test if it works, can be removed later
                 Debug.Log("Placement!");
+                
+                //Calls function from the LifeCapacity script to increase max capacity by 50
+                lifeCapacity.IncreaseCapacity();
             }  
         }
     }
--- Assets/Scripts/Stats/LifeCapacity.cs
+++ Assets/Scripts/Stats/LifeCapacity.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class LifeCapacity : MonoBehaviour
 {
+    //Max population
     [SerializeField] 
     private int maxCapacity;
 
+    //Current population
     [SerializeField] 
     private int currentPop;
 
+    //Counter to determine interval between growth or decline
     [SerializeField]
     private float counter = 5f;
 
@@ -22,12 +25,16 @@ public class LifeCapacity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Counter counts down
         counter -= Time.deltaTime;
         
+        //Start of game, when there is 0 capacity, the population will count down by one every 5 seconds
         if (currentPop > maxCapacity)
         {
             DyingPop();
         }
+        
+        //Once building is placed, population grows at the same interval until it reaches max capacity
         else if (currentPop <= maxCapacity)
         {
             GrowingPop();
@@ -53,5 +60,11 @@ public class LifeCapacity : MonoBehaviour
 
             counter = 5;
         }
+    }
+
+    //To be called once building is placed, increases the maximum capacity.
+    public void IncreaseCapacity()
+    {
+        maxCapacity += 50;
     }
 }
